e1f38b4b847eb078132c5163da455a50f1e4b28c
hamidred <136030374+hamidred@users.noreply.github.com>
2023-10-20 22:12:05 +0100

    Update main.c

--- main.c
+++ main.c
@@ -40,6 +40,7 @@ _printf("Len:[%d]\n", len);
 printf("Len:[%d]\n", len2);
 _printf("Unknown:[%r]\n");
 printf("Unknown:[%r]\n");
+_printf("%S\n", "Best\nSchool");
 return (0);
 }
 /*Collaborators: Juskins & MCCHANCE */
